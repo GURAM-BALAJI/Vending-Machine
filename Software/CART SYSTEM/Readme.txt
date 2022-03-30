da1cf7cdca4890ce1694e2b1dbfadc1057b3b667
pradeep-r-s <100504026+pradeep-r-s@users.noreply.github.com>
2022-03-31 01:07:30 +0530

    SHOPPING CART
    cart system

---
+++ Software/CART SYSTEM/Readme.txt
@@ -0,0 +1,7 @@
+AUTHOR : PRADEEP R.S
+
+
+shopping cart for vending machine.
+
+
+managing the items easily with help of this system.
---
+++ Software/CART SYSTEM/vending-shopping-cart/src/data.js
@@ -0,0 +1,35 @@
+const data = {
+    products : [
+        {
+            id: "1",
+            name: "LAYS",
+            price: 10,
+            image:"https://www.bigbasket.com/media/uploads/p/l/294297_16-lays-potato-chips-calm-cream-onion-flavour.jpg",
+        },
+        {
+            id: "2",
+            name: "OREO",
+            price: 20,
+            image:"https://www.bigbasket.com/media/uploads/p/l/277584-2_11-cadbury-oreo-creme-biscuit-vanilla-original.jpg",
+        },
+        {
+            id: "3",
+            name: "DORITOS",
+            price: 20,
+            image:"https://m.media-amazon.com/images/I/71MQeIS7FAL._SL1500_.jpg"
+        },
+        {
+            id: "4",
+            name: "KURKURE",
+            price: 10,
+            image:"https://th.bing.com/th/id/OIP.c0WiMC0DEy7PUjku8-BZoAHaHa?pid=ImgDet&rs=1"
+        },
+        {
+            id: "5",
+            name: "BOUR BON",
+            price: 40,
+            image:"https://karthika.sg/image/catalog/products/CookiesBiscuits/400058.jpg"
+        },
+    ],
+};
+export default data;
